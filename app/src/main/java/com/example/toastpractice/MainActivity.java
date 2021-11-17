19b41e6bdb78293fe484fcbf1a40f5ff0b274d2a
anas012 <anas@salesassist.pk>
2021-11-17 22:22:12 +0500

    Creating alert Dialogue Box

--- app/src/main/java/com/example/toastpractice/MainActivity.java
+++ app/src/main/java/com/example/toastpractice/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.toastpractice;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn=findViewById(R.id.toastbtn);
+        Button alertbtn=findViewById(R.id.alertbtn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,6 +26,35 @@ public class MainActivity extends AppCompatActivity {
 
                 toast.setGravity(Gravity.CENTER|Gravity.RIGHT,100,200);
                 toast.show();
+            }
+        });
+        alertbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Alert");
+                builder.setMessage("Do u want to leave us?");
+                builder.setCancelable(false);
+                builder.setPositiveButton(
+                        "Yes",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        }
+                );
+                builder.setNegativeButton(
+                        "No",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        }
+                );
+                AlertDialog alertDialog=builder.create();
+                alertDialog.show();
             }
         });
     }
