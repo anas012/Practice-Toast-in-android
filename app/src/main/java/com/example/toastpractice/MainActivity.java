d0c5b3155afb72799b5cb6ef2f07691ea47d0bef
anas012 <anas@salesassist.pk>
2021-11-17 21:36:32 +0500

    btn function created

--- app/src/main/java/com/example/toastpractice/MainActivity.java
+++ app/src/main/java/com/example/toastpractice/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.toastpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btn=findViewById(R.id.toastbtn);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(this,"Hello user!How R you?",)
+            }
+        });
     }
 }
